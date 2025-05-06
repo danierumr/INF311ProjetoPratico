b2c245af7e6800d784f028543d720956a2f883c2
Daniel <51673798+danierumr@users.noreply.github.com>
2025-05-06 13:36:02 -0300

    Ajuste e criacao do projeto

---
+++ app/src/main/java/inf311/grupo1/projetopratico/MainActivity.java
@@ -0,0 +1,14 @@
+package inf311.grupo1.projetopratico;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
