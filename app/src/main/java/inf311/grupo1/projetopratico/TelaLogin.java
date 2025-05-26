d56d9e514d3ad8d21f99ca22d40ad26d0ff30f5c
gabriel-cmf <gabriel.fernandes1@ufv.br>
2025-05-26 14:15:50 -0300

    Implementação tela de novo lead

--- app/src/main/java/inf311/grupo1/projetopratico/TelaLogin.java
+++ app/src/main/java/inf311/grupo1/projetopratico/TelaLogin.java
@@ -1,5 +1,6 @@
 package inf311.grupo1.projetopratico;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.View;
@@ -17,8 +18,8 @@ public class TelaLogin extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_login);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        //StrictMode.setThreadPolicy(policy);
 
         Button btn2 = (Button) findViewById(R.id.user_btn);
         //btn2.setBackgroundColor(getResources().getColor(R.color.white));
@@ -35,5 +36,13 @@ public class TelaLogin extends AppCompatActivity
     public void set_as_admin(View v)
     {
         is_admin=true;
+    }
+
+    public void to_dashboard(View v)
+    {
+        Intent a = new Intent(getBaseContext(), TelaNovoLead.class);
+        a.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        startActivity(a);
     }
 }
