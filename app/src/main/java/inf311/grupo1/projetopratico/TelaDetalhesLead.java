de3c5ab847bc28afe4cf12d2e4d03296a193e04b
gabriel-cmf <gabriel.fernandes1@ufv.br>
2025-06-05 21:36:45 -0300

    Implementação parcial do Detalhes do lead

---
+++ app/src/main/java/inf311/grupo1/projetopratico/TelaDetalhesLead.java
@@ -0,0 +1,21 @@
+package inf311.grupo1.projetopratico;
+
+import android.os.Bundle;
+
+public class TelaDetalhesLead extends Toolbar_activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tela_detalhes_lead);
+
+        //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        // StrictMode.setThreadPolicy(policy);
+
+
+
+
+    }
+}
