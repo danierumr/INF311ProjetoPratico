713adec8e9b727093eba681cf5d7984e19047c01
gabriel-cmf <gabriel.fernandes1@ufv.br>
2025-06-07 10:59:26 -0300

    Implementação dos contatos para o dashboard, melhorias nas duas atividades

--- app/src/main/java/inf311/grupo1/projetopratico/TelaDetalhesLead.java
+++ app/src/main/java/inf311/grupo1/projetopratico/TelaDetalhesLead.java
@@ -1,6 +1,8 @@
 package inf311.grupo1.projetopratico;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class TelaDetalhesLead extends Toolbar_activity
 {
@@ -8,11 +10,39 @@ public class TelaDetalhesLead extends Toolbar_activity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_detalhes_lead);
 
         //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         // StrictMode.setThreadPolicy(policy);
+
+        Intent intent = getIntent();
+        Contato c = intent .getParcelableExtra("contato");
+
+        TextView nome = findViewById(R.id.detalhes_lead_name);
+        nome.setText(c.nome);
+
+
+
+        TextView escola_serie = findViewById(R.id.detalhes_lead_escola);
+
+        escola_serie.setText(c.escola + " • " + c.serie);
+
+
+        TextView responsavel = findViewById(R.id.detalhes_lead_resp_edit);
+        responsavel.setText(c.responsavel);
+
+        TextView email = findViewById(R.id.detalhes_lead_email_edit);
+        email .setText(c.email);
+
+        TextView telefone = findViewById(R.id.detalhes_lead_tel_edit);
+        telefone.setText(c.telefone);
+
+        TextView interesse = findViewById(R.id.detalhes_lead_interesse_edit);
+        interesse.setText(c.interesse);
+
+
 
 
 
